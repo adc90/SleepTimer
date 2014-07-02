9e68b4dee2314bb030036400437af5a16e7d1955
adc90 <aaronc81590@gmail.com>
2014-07-02 14:55:12 -0400

    Fixed issues where the processes wouldn't close. The program's user input needs to be idiot proofed. The program also needs to be updated to deal with programs being manually shut down.

--- SleepTimer.pl
+++ SleepTimer.pl
@@ -1,46 +1,43 @@
+=pod
+Author: Aaron Clevenger
+Desc: Simple sleep timer for linux using perl.
+=cut
+
 use strict;
 use warnings;
 use Proc::Simple;
 
-sub main
-{
-	$| = 1; #Flush the STD input
-	
 my $hdr = << "EOF"; 
 //----------------------------------
 //  Sleep Timer
 //----------------------------------
 EOF
 
+sub main
+{
+	$| = 1; #Flush the STD input
+
 	print $hdr;
-	print "Please enter the command you want to put on a timer: \n";
+	print "Please enter the command you want to put on a timer: ";
 	my $proc = <STDIN>;
-
-	my $status = system($proc);
-
-	print $status;
 
 	print "Set sleep mode ('T'imer or 'A'larm): ";
 	my $input = lc(<STDIN>);
 	chomp $input;
 	
-	my $p = Proc::Simple->new();
-	$p->start("gvim");
-	sleep(4);
-	$p->kill();
 
-	if($input eq "a")
-	{
-		sleep_alarm();
+	if($input eq "a"){
+		sleep_alarm($proc);
 	}elsif($input eq "t"){
-		sleep_timer();	
+		sleep_timer($proc);	
 	}else{
-		return 0; #Should ask again really
+		main();	
 	}
 }
 
 sub sleep_timer
 {
+	my $proc = $_[0];
 	print "Please enter the amount of time you'd like your program to run (hh:mm): ";
 	my $input = <STDIN>;
 
@@ -49,15 +46,15 @@ sub sleep_timer
 	my $hrs = $time[0] * 3600;
 	my $min = $time[1] * 60;
 
-	my @time = localtime();
-	
+	my $p = Proc::Simple->new();
+	$p->start($proc);
 	sleep($hrs + $min);
-
-	print "End of Sleep Timer";
+	$p->kill();
 }
 
 sub sleep_alarm
 {
+	my ($proc) = @_;
 	print "Please enter the time you'd like the program to shut off (hh::mm) ";
 	my $input = <STDIN>;
 
@@ -69,6 +66,9 @@ sub sleep_alarm
 	chomp($hrs);
 	chomp($min);
 	my $alarm_time = join(':',$hrs,$min);
+	
+	my $p = Proc::Simple->new();
+	$p->start($proc);
 
 	my $end = 1;
 	while($end != 0){
@@ -79,7 +79,7 @@ sub sleep_alarm
 			$end = 0;
 		}
 	}
-	print "End";
+	$p->kill();
 	
 }
 
